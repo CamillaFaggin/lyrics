3ee831f850d47d8a1575334fb1e36738797bd85d
Cami9 <73961838+Cami9@users.noreply.github.com>
2020-11-30 12:19:40 +0100

    modified main in .py

---
+++ Function_main.py
@@ -0,0 +1,35 @@
+import json
+import requests
+def get_lyrics():
+    base_url = "https://api.lyrics.ovh/v1/"
+    
+    print()
+    
+    artist_name=input('Write the artist name here: ')
+    song_title=input('Write the song name here: ')
+    
+    search_url = base_url + artist_name + '/' + song_title
+    
+    print()
+    print()
+    
+    req = requests.get(search_url)
+    dict_lyrics=req.json()
+    list_lyr=dict_lyrics['lyrics']
+    
+    if len(list_lyr) == 0 :
+        print("We haven't found a lyrics for your research.")
+        print("Please verify that you haven't mispelled, or try changing your song.")    
+        
+        #artist_name2=input('Write the artist name here: ')
+        #song_title2=input('Write the song name here: ')
+        #break
+        
+    else:
+        print('The lyrics is: ')
+        
+    
+    print( list_lyr )
+
+print(get_lyrics())
+
