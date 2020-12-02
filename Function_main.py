ec0e4e2714470220d60d18b2906b4be90eb84505
Giulia Angelini <>
2020-12-02 12:34:14 +0100

    saving after merge

--- Function_main.py
+++ Function_main.py
@@ -1,6 +1,22 @@
 import json
 import requests
 import csv
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("get info", help="The function gives the lyrics of a song n times")
+    args = parser.parse_args()
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x")
+    args = parser.parse_args()
+    print(args.x) 
+
+    for i in range (int(args.x)):
+        print(i+1,'song')
+        get_lyrics()
 
 def get_lyrics():
     base_url = "https://api.lyrics.ovh/v1/"
@@ -39,14 +55,13 @@ def get_lyrics():
     if len(list_lyr) == 0 :
         print("We haven't found a lyrics for your research.")
         print("Please verify that you haven't mispelled, or try changing your song.")    
-        
-        #artist_name2=input('Write the artist name here: ')
-        #song_title2=input('Write the song name here: ')
-        #break
+           
         
     else:
         print('The lyrics is: ')
         
     
-print(get_lyrics())
+    print( list_lyr )
+    
 
+main()
