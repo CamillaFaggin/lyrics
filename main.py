4c3751d1c883e979ef28f08fc64b2827fac07e4f
dayanamanfredi <dayanamanfredi@student.h-farm.com>
2020-12-14 19:32:32 +0100

    modify main according to pep8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''import libraries and modules''' 
+'''import libraries and modules'''
 import json
 import requests
 import csv
@@ -8,17 +8,19 @@ from itertools import islice
 from operator import itemgetter
 
 '''implementing argparse'''
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("get info", help="Firstly, Insert a n. of songs you want")
-    parser.add_argument("-- verbosity", help="Consequently, n. lyrics are displayed.",
+    parser.add_argument("get info", help="Insert a n. of songs you want")
+    parser.add_argument("-- verbosity", help="n. lyrics are displayed.",
                         action="store_true")
     args = parser.parse_args()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("x")
-    args = parser.parse_args() 
-    print(args.x) 
+    args = parser.parse_args()
+    print(args.x)
 
     for i in range(int(args.x)):
         print(i+1, 'song')
@@ -27,7 +29,7 @@ def main():
 
 def get_lyrics():
     base_url = "https://api.lyrics.ovh/v1/"
-#define the input values
+#  define the input values
     artist_name = input('Write the artist name here: ')
     song_title = input('Write the song name here: ')
 
@@ -36,22 +38,21 @@ def get_lyrics():
     status = req.status_code
     if status == 404:
         print("Invalid research. Retry")
-    
-    else: 
+    else:
         dict_lyrics = req.json()
         list_lyr = dict_lyrics['lyrics']
-        
         if len(list_lyr) == 0:
             print("We haven't found a lyrics for your research.")
-            print("Please verify that you haven't mispelled, or try changing your song.")
+            print("Verify that you haven't mispelled, or change your song.")
         else:
             with open('lyrics.csv', 'w', newline='') as file:
-                 writer = csv.writer(file)
-                 writer.writerow(["artist", "song", "lyrics"])
-                 writer.writerow([artist_name, song_title, list_lyr])
+                writer = csv.writer(file)
+                writer.writerow(["artist", "song", "lyrics"])
+                writer.writerow([artist_name, song_title, list_lyr])
 
-            csv.register_dialect('csv_dialect', delimiter='|', skipinitialspace=False,
-                                     quoting=csv.QUOTE_ALL)
+            csv.register_dialect('csv_dialect', delimiter='|',
+                                 skipinitialspace=False,
+                                 quoting=csv.QUOTE_ALL)
 #  first 5 only
             with open('lyrics.csv', 'r') as csvfile:
                 reader = csv.reader(csvfile, delimiter=" ", quotechar='|')
@@ -59,16 +60,17 @@ def get_lyrics():
                     print(', '.join(row))
 
         def fun_sqlite():
-            list_lyr2 =list_lyr.replace('\r\n ', ' ')
-            list_lyr2 =list_lyr2.replace('\n ', ' ')
-            list_lyr2 =list_lyr2.replace('\r', ' ')
-            list_lyr3 =list_lyr2                     
-        
+            list_lyr2 = list_lyr.replace('\r\n ', ' ')
+            list_lyr2 = list_lyr2.replace('\n ', ' ')
+            list_lyr2 = list_lyr2.replace('\r', ' ')
+            list_lyr3 = list_lyr2
             conn1 = sqlite3.connect('lyrics.db')
             curs1 = conn1. cursor()
-            #curs1.execute('''CREATE TABLE tablelyr1 (artist_name, song_title, lyrics)''')
-            curs1.execute("INSERT INTO tablelyr1 VALUES (?,?,?)",  (artist_name, song_title, list_lyr3))
-            r = curs1.execute('SELECT * FROM tablelyr1').fetchall() 
+#  curs1.execute('''CREATE TABLE tablelyr1,
+#  (artist_name, song_title, lyrics)''')
+            curs1.execute("INSERT INTO tablelyr1 VALUES (?,?,?)",
+                          (artist_name, song_title, list_lyr3))
+            r = curs1.execute('SELECT * FROM tablelyr1').fetchall()
             print()
             for row2 in r:
                 print (row2)
@@ -78,13 +80,6 @@ def get_lyrics():
             print('Database from sqlite')
         fun_sqlite()
 
-fun_sqlite=get_lyrics
-
-
+fun_sqlite = get_lyrics
 
 main()
-
-
-
-
-
