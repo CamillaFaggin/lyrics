1cb4ad91264124917948def2a7ef8b6968dfbbc8
dayanamanfredi <dayanamanfredi@student.h-farm.com>
2020-12-14 19:56:11 +0100

    control pep8

--- test_lyrics/test.py
+++ test_lyrics/test.py
@@ -9,16 +9,12 @@ class TestInput(unittest.TestCase):
     # invalid inputs
     def test_wrong_values(self):
         # you should input wrong data
-        self.assertEqual(get_lyrics("artist", "song"), None )
-    
+        self.assertEqual(get_lyrics("artist", "song"), None)
     # corner case: empty string
+
     def test_empty_string(self):
         self.assertEqual(get_lyrics(" ", " "), None)
         self.assertEqual(get_lyrics("Coldplay", " "), None)
         self.assertEqual(get_lyrics(" ", "Yellow"), None)
-            
-
-
-if __name__ == '__test.py__' :
-    
+if __name__ == '__test.py__':
     unittest.get_lyrics()
